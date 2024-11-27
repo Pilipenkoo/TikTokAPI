3aa58161c3724cdb7ff8008ee48338ee45424e65
Pilipnkoo <pilipenko437@gmail.com>
2024-11-27 23:38:31 +0300

    message sending fix(send messages)

--- use_cases/message_sending.py
+++ use_cases/message_sending.py
@@ -19,9 +19,19 @@ class MessageSendingUseCase:
             # Ожидаем появления окна чата и находим строку ввода
             locator_input = {"by": "xpath", "value": "//div[@contenteditable='true' and @role='textbox']"}
             message_input = self.web_interface.wait_until_present(locator_input, timeout=15)
+
+            # Прокручиваем страницу до элемента
+            self.web_interface.execute_script("arguments[0].scrollIntoView(true);", message_input)
+
+            # Если есть плейсхолдер, скрываем его с помощью JavaScript
+            placeholder = self.web_interface.find_element(
+                {"by": "xpath", "value": "//div[contains(@class, 'public-DraftEditorPlaceholder-inner')]"})
+            self.web_interface.execute_script("arguments[0].style.visibility='hidden';", placeholder)
+
+            # Кликаем на поле ввода
             self.web_interface.click_element(message_input)
 
-            # Проверка, что поле ввода готово
+            # Проверяем, что поле ввода активировано
             if message_input.is_enabled():
                 # Вводим сообщение
                 message_input.send_keys(message_text)
@@ -31,5 +41,3 @@ class MessageSendingUseCase:
                 print(f"Поле ввода не доступно для пользователя {follower.username}")
         except Exception as e:
             print(f"Не удалось отправить сообщение {follower.username}: {str(e)}")
-
-
