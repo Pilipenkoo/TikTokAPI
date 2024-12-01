497ee99e40b02815696fc1b0854e01d848b4ae19
Pilipnkoo <pilipenko437@gmail.com>
2024-12-01 12:55:33 +0300

    Add friends in subscribe list

--- use_cases/followed_management.py
+++ use_cases/followed_management.py
@@ -1,6 +1,5 @@
 # use_cases/follower_management_use_case.py
 import json
-
 import time
 from entities.follower import Follower
 from interfaces.web_interface import WebInterface
@@ -27,7 +26,7 @@ class FollowerManagementUseCase:
             # Прокручиваем список и собираем подписчиков
             followers_set = set()
             last_count = 0
-            max_attempts = 5
+            max_attempts = 1
             attempts = 0
 
             while True:
@@ -35,14 +34,25 @@ class FollowerManagementUseCase:
                 self.web_interface.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", scrollable_div)
                 time.sleep(2)  # Ждем загрузки новых элементов
 
+                # Нажимаем на кнопки "Добавить в ответ"
+                add_buttons = self.web_interface.find_elements({
+                    "by": "xpath",
+                    "value": "//button[contains(@aria-label, 'Добавить в ответ') and @data-e2e='follow-button']"
+                })
+
+                for button in add_buttons:
+                    try:
+                        self.web_interface.click_element(button)
+                        time.sleep(1)  # Небольшая пауза между нажатиями
+                    except Exception as e:
+                        print(f"Не удалось нажать на кнопку 'Добавить в ответ': {e}")
+
                 # Собираем элементы подписчиков
                 followers_elements = self.web_interface.find_elements({
                     "by": "xpath",
                     "value": "//li//a[contains(@class, 'StyledLink-StyledUserInfoLink')]"
                 })
                 current_count = len(followers_elements)
-                # print(followers_elements)
-                # print(f"Найдено {current_count} элементов подписчиков.")
 
                 # Проверяем, достигли ли конца списка
                 if current_count == last_count:
@@ -55,15 +65,10 @@ class FollowerManagementUseCase:
 
                 # Собираем данные о подписчиках
                 for elem in followers_elements:
-
                     href = elem.get_attribute("href")
-                    print(href)
-                    # print(href)
                     if href.startswith('https://www.tiktok.com'):
                         profile_url = href
-                        print(profile_url)
                         username = href.split('@')[-1]
-                        print(username)
                         followers_set.add(Follower(username, profile_url))
 
             return followers_set
@@ -73,5 +78,3 @@ class FollowerManagementUseCase:
             import traceback
             traceback.print_exc()
             return set()
-
-
